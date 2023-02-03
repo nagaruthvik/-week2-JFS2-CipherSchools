4be370dd64b78ca950033b53357f4b0e8c8d112f
naga ruthvik <94837055+nagaruthvik@users.noreply.github.com>
2023-02-03 12:03:46 +0530

    Create Command-Line.java

---
+++ Command-Line.java
@@ -0,0 +1,19 @@
+class father{
+    public final String lastName="obroi";
+    public final void foodHabit(){
+        System.out.println("I like Salty");
+    }
+}
+class son extends father{
+    public void foodHabit(){
+        lastName="Malholra";
+        System.out.println("I like Sweet");
+
+    }
+
+}
+public class Final {
+    public static void main(String[] args){
+
+    }
+}
