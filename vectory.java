4c36073802eb3011070d05e95110f9814a9e05c5
naga ruthvik <94837055+nagaruthvik@users.noreply.github.com>
2023-02-04 18:48:11 +0530

    Create vectory.java

---
+++ vectory.java
@@ -0,0 +1,15 @@
+import java.util.Iterator;
+import java.util.Vector;
+
+public class vector {
+    public static void main(String[] args) {
+        Vector vec = new Vector();
+        vec.add("texas");
+        vec.add("zurich");
+        vec.add("newyork");
+        Iterator pointer = vec.iterator();
+        while (pointer.hasNext()){
+            System.out.println(pointer.next());
+        }
+    }
+}
